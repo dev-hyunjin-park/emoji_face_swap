e77d8d7c2ef6a5ea913921cea255b12e98aa064d
“박현진” <dev.hyunjin.park@gmail.com>
2024-01-13 14:27:06 +0900

    multiplePose

--- sketch.js
+++ sketch.js
@@ -1,27 +1,22 @@
 let video;
 let faceImage;
 
+let isVideoPlaying = false;
+
 let poseNet;
 let poses = [];
 
-let ready = false;
-
-function drawFace() {
-  image(video, 0, 0, width, height);
-}
-
 function modelLoaded() {
   console.log("Model Loaded!");
+  poseNet.multiPose(video);
 }
 
 function drawVideo() {
-  image(video, 0, 0, 640, 480);
-  strokeWeight(2);
+  image(video, 0, 0, width, height);
 
-  // For one pose only (use a for loop for multiple poses!)
-  if (poses.length > 0) {
-    const pose = poses[0].pose;
-
+  for (let i = 0; i < poses.length; i++) {
+    const pose = poses[i].pose;
+    console.log(poses[i]);
     const nose = pose["nose"];
     const eyeL = pose["leftEye"];
     const eyeR = pose["rightEye"];
@@ -33,9 +28,7 @@ function drawVideo() {
     const shourlderR = pose["rightShoulder"];
 
     const faceWidthDistance = dist(earR.x, earR.y, earL.x, earL.y) * 0.03;
-
     const faceWidth = (earR.x - earL.x) * faceWidthDistance;
-    // console.log(faceWidthDistance);
 
     const faceHeightDistance =
       Math.max(
@@ -57,21 +50,25 @@ function drawVideo() {
 
 function videoLoaded() {
   console.log("video loaded");
-
+  video.size(width, height);
+  video.elt.muted = true;
   video.play();
+  isVideoPlaying = true;
+  video.onended(() => (isVideoPlaying = false));
+
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on("pose", (results) => {
-    poses = results;
-    drawVideo();
+    if (isVideoPlaying) {
+      poses = results;
+      drawVideo();
+    }
   });
 }
 
 function setup() {
   createCanvas(640, 480);
   faceImage = loadImage("smile.png");
-
   video = createVideo("video.mov", videoLoaded);
-  video.size(640, 480);
   video.hide();
 }
 
