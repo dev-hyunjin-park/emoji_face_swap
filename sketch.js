26f7de923f9c54989878782c4dcde98384873878
“박현진” <dev.hyunjin.park@gmail.com>
2024-01-13 10:05:43 +0900

    라이브러리 변경(faceApi -> PoseNet) 얼굴이 측면으로 너무 돌아가면 감지안되는 경우 때문에 포즈넷으로 변경

--- sketch.js
+++ sketch.js
@@ -1,53 +1,63 @@
 let video;
-let faceapi;
 let faceImage;
 
-function drawFace(result) {
+let poseNet;
+let poses = [];
+
+let ready = false;
+
+function drawFace() {
   image(video, 0, 0, width, height);
-
-  const { _x, _y, _width, _height } =
-    result[result.length - 1].alignedRect._box;
-
-  const margin = 40;
-  const enlargedWidth = _width + 2 * margin;
-  const enlargedHeight = _height + 2 * margin;
-
-  image(faceImage, _x - margin, _y - margin, enlargedWidth, enlargedHeight);
-}
-
-function gotResults(error, result) {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  if (result && result.length > 0) {
-    drawFace(result);
-  }
-  faceapi.detect(video, gotResults);
 }
 
 function modelLoaded() {
   console.log("Model Loaded!");
-  faceapi.detect(video, gotResults);
+}
+
+function drawVideo() {
+  image(video, 0, 0, 640, 480);
+  strokeWeight(2);
+
+  // For one pose only (use a for loop for multiple poses!)
+  if (poses.length > 0) {
+    let pose = poses[0].pose;
+
+    // Create a pink ellipse for the nose
+    fill(213, 0, 143);
+    let nose = pose["nose"];
+    ellipse(nose.x, nose.y, 20, 20);
+
+    // Create a yellow ellipse for the right eye
+    fill(255, 215, 0);
+    let rightEye = pose["rightEye"];
+    ellipse(rightEye.x, rightEye.y, 20, 20);
+
+    // Create a yellow ellipse for the right eye
+    fill(255, 215, 0);
+    let leftEye = pose["leftEye"];
+    ellipse(leftEye.x, leftEye.y, 20, 20);
+  }
 }
 
 function videoLoaded() {
   console.log("video loaded");
 
-  const detectionOptions = {
-    withLandmarks: true,
-    withDescriptors: false,
-  };
-  faceapi = ml5.faceApi(detectionOptions, modelLoaded);
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose", (results) => {
+    poses = results;
+    drawVideo();
+  });
 }
 
 function setup() {
   createCanvas(640, 480);
   faceImage = loadImage("smile.png");
 
-  video = createCapture(VIDEO, videoLoaded);
-  video.size(width, height);
+  video = createVideo("phoebe.mov", videoLoaded);
+  video.size(640, 480);
+
+  video.showControls();
+  video.loop();
   video.hide();
 }
 
